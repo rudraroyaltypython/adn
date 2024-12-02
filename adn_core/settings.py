bf0a0107bc231704d7fdc4466bd5f8c6c6df93d8
rudraroyaltypython <151947434+rudraroyaltypython@users.noreply.github.com>
2024-12-02 13:07:14 +0530

    setting file updated

--- adn_core/settings.py
+++ adn_core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-914-82+_eeg!fo_g0!a0#+wi3btl@rp&4o$zj)9##ytt-i+a8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
